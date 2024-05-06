f152e4429870780368a8249da260bb88c10e0878
DoKyeongyun <aservmz@naver.com>
2024-05-06 22:25:29 +0900

    feat: 기존 스크롤뷰 UI 클래스 분리

--- lib/food_map.dart
+++ lib/food_map.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:zido/food.dart';
-import 'package:zido/food_item.dart';
-import 'package:zido/user.dart';
+import 'package:zido/food_item_scroll_view.dart';
 
 class FoodMap extends StatelessWidget {
   const FoodMap({
@@ -14,43 +12,7 @@ class FoodMap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            toolbarHeight: 50,
-            title: Text(
-              title,
-              style: const TextStyle(fontSize: 16.0),
-            ),
-            snap: false,
-            floating: true,
-            pinned: false,
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                FoodItem(
-                  user: const User(
-                    nickname: 'Man',
-                    image: AssetImage("assets/images/man.png"),
-                  ),
-                  writer: const User(
-                    nickname: 'Woman',
-                    image: AssetImage("assets/images/woman.png"),
-                  ),
-                  content: 'Very delicious food!',
-                  food: const Food(
-                    name: '닭날개+참치밥+계란찜',
-                    icon: AssetImage("assets/images/roast-chicken.png"),
-                    imagePath: 'assets/images/our-food-1.jpeg',
-                  ),
-                  createdDate: DateTime.now(),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      body: FoodItemScrollView(title: title),
     );
   }
 }
